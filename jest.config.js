dd49bf57a7da4c17da0f4a5e0dad32f1d60b320f
Gabriel Ávila <avilabiel@gmail.com>
2021-05-26 13:17:49 -0300

    so now we check the repository type

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,5 @@ module.exports = {
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1",
   },
-  setupFilesAfterEnv: ["<rootDir>/setupEnvironment.jest.js"],
+  // setupFilesAfterEnv: ["<rootDir>/setupEnvironment.jest.js"],
 };

f284ce88093fd4f60d9522a55553ae9678377dc3
GGotha <snakygamer@gmail.com>
2021-05-26 11:54:36 -0300

    fix express

--- src/externals/Express.ts
+++ src/externals/Express.ts
@@ -1,8 +1,17 @@
-import Express from "express";
-import { Request, Response, Error } from 'express';
+import express, { Express, Request, Response, NextFunction } from "express";
+
+import path from "path";
+import dotenv from "dotenv";
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+dotenv.config({
+  path: path.resolve(__dirname, "..", "..", ".env." + process.env.NODE_ENV),
+});
+
 import bodyParser from "body-parser";
 import Youch from "youch";
-const helmet = require("helmet");
+import helmet from "helmet";
 import InvalidRoutes from "../adapters/routes/Express/InvalidRoutes";
 import Routes from "../adapters/routes/Express/Routes";
 import ServiceLocator from "./ServiceLocator";
@@ -11,7 +20,7 @@ export default class Server {
   public express: Express;
 
   constructor() {
-    this.express = Express();
+    this.express = express();
 
     this.middlewares();
     this.routes();
@@ -28,7 +37,7 @@ export default class Server {
     this.express.use(this.buildServiceLocator);
   }
 
-  buildServiceLocator(req, res, next): void {
+  buildServiceLocator(req: Request, res: Response, next: NextFunction): void {
     req.serviceLocator = ServiceLocator.getInstance();
     return next();
   }
@@ -41,20 +50,22 @@ export default class Server {
     this.express.use(InvalidRoutes);
   }
 
-  exception(): Response {
-    this.express.use(async (err: Error, req: Request, res: Response, next: Function) => {
-      let message: any = "Internal Server Error";
+  exception(): void {
+    this.express.use(
+      async (err: Error, req: Request, res: Response, next: NextFunction) => {
+        let message: any = "Internal Server Error";
 
-      if (process.env.NODE_ENV !== "production") {
-        const youch = new Youch(err, req);
-        message = await youch.toJSON();
+        if (process.env.NODE_ENV !== "production") {
+          const youch = new Youch(err, req);
+          message = await youch.toJSON();
+        }
+
+        return res.status(500).send({ success: false, message });
       }
-
-      return res.status(500).send({ success: false, message });
-    });
+    );
   }
 
-  createHTTPServer() {
+  createHTTPServer(): void {
     this.express.listen(process.env.PORT);
     console.log(`Server running at port ${process.env.PORT}`);
   }
